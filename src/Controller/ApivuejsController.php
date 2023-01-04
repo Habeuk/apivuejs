98535fb88f634f6d1cbee85c3f44520f904f0c28
kouwa stephane <kksasteph888@gmail.com>
2023-01-04 19:55:47 +0100

    Maj

--- src/Controller/ApivuejsController.php
+++ src/Controller/ApivuejsController.php
@@ -3,6 +3,10 @@
 namespace Drupal\apivuejs\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Stephane888\Debug\ExceptionExtractMessage;
+use Drupal\Component\Serialization\Json;
+use Symfony\Component\HttpFoundation\Request;
+use Stephane888\DrupalUtility\HttpResponse;
 
 /**
  * Returns responses for Api vuejs routes.
@@ -12,14 +16,21 @@ class ApivuejsController extends ControllerBase {
   /**
    * Builds the response.
    */
-  public function build() {
-
-    $build['content'] = [
-      '#type' => 'item',
-      '#markup' => $this->t('It works!'),
-    ];
-
-    return $build;
+  public function saveEntity(Request $Request, $entity_type_id): \Symfony\Component\HttpFoundation\JsonResponse
+  {
+    try {
+      $defaultValues = Json::decode($Request->getContent());
+      $entity = $this->entityTypeManager()->getStorage($entity_type_id)->create($defaultValues);
+      $entity->save();
+      return HttpResponse::response([
+        'id' => $entity->id(),
+        'json' => $entity->toArray()
+      ]);
+    }
+    catch (\Exception $e) {
+      $this->getLogger('buildercv')->critical($e->getMessage() . '<br>' . ExceptionExtractMessage::errorAllToString($e));
+      return HttpResponse::response(ExceptionExtractMessage::errorAll($e), 400, $e->getMessage());
+    }
   }
 
 }
